6b2b884da10ff01fd000c48783fbfd5e6bb9a2fc
Dalia <dalia.garcia16@hotmail.com>
2020-01-05 12:30:48 -0300

    quinto commit proyecto team running

--- themes/mi_tema/single.php
+++ themes/mi_tema/single.php
@@ -3,16 +3,31 @@
 <?php if ( have_posts() ) { ?>
 	<?php while ( have_posts() ) { ?>
 		<?php the_post(); ?>
+<main class="SectionMainPlantilla py-5">
+	<article class="mb-5 container py-5 my-5">
+		<div class="text-center my-3">
+			<p><i class="fas fa-calendar-alt"></i> <time
+					datetime="<?php the_time('Y-m-d') ?>"><?php the_time('d \d\e F \d\e Y') ?>
+				</time></p>
+				<h2><?php the_title() ?></h2>
+				<?php the_post_thumbnail('detail', array('class'
+				=> 'w-100 h-auto my-4')); ?>
+				<h4><?php the_category(' '); ?></h4>
+</div>
 
-		<?php the_post_thumbnail() ?>
-		<?php the_title() ?>
-		<time datetime="<?php the_time('Y-m-d') ?>"><?php the_time('d \d\e F \d\e Y') ?></time>
-		<?php the_content() ?>
+<div class="SectionMainPlantilla__content">
+   <?php the_content() ?>
+	</div>
 
+	<div class="text-center mb-5">
+        <a href="blog.html" class="btn btn-warning btn-lg mt-5">Volver al Blog</a>
+      </div>
+</article>
+</main>
 	<?php } ?>
 <?php } else { ?>
 	<!-- Content -->
 <?php } wp_reset_query(); ?>
 
-<?php get_sidebar() ?>
+
 <?php get_footer() ?>

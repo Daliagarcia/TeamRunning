0d21a748166af987bc72c733a921278a0d577549
Dalia <dalia.garcia16@hotmail.com>
2020-01-06 13:37:17 -0300

    commit trece proyecto team running

--- themes/mi_tema/404.php
+++ themes/mi_tema/404.php
@@ -1,6 +1,12 @@
 <?php get_header() ?>
-
-	<h1>Error 404:</h1>
-	<h2>Página no encontrada</h2>
+<?php the_post() ?>
+<div class="py-5">
+		<div class="container text-center">
+			<?php get_template_part('_includes/main', 'section') ?>
+			<h3 class="text-center">Error 404:</h3>
+			<h4 class="text-center">Página no encontrada</h4>
+			<p class="text-center my-5"><a href="<?php echo get_the_permalink( $post = 8, $leavename = false );?>" class="btn btn-warning btn-lg">Volver a la página de Inicio</a></p>
+		</div>
+	</div>
 
 <?php get_footer() ?>
--- themes/mi_tema/footer.php
+++ themes/mi_tema/footer.php
@@ -1,7 +1,15 @@
-<?php if ( has_nav_menu( 'footer-menu' ) ) { ?>
-	<?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container_class' => 'footer-menu' ) ); ?>
-<?php } ?>
+<footer class="footer text-center">
+    <a href="https://www.facebook.com/teammora/"><i class="fab fa-facebook-square fa-2x"></i>TEAMMORA</a>
+    <a href="https://www.instagram.com/teammorachile/"><i class="fab fa-instagram fa-2x"></i>TEAMMORACHILE</a>
 
-<?php wp_footer() ?>
+</footer>
+
+<!-- Jquery V.3.4.1 -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<!-- Popper JS V.1.16.0 -->
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<!-- Bootstrap JS V.4.4.1 -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
+
 </html>
